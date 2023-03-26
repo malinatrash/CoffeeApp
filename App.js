143e185062a3ca3ab08c030cc6469ee72eb59a29
Pavel <malinatrashh@gmail.com>
2023-03-26 21:41:56 +0800

    Реализовал страницу с детальной информацией о кофе

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
-import colors from './app/config/colors'
-import HomeScreen from "./HomeScreen";
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View } from "react-native"
+import colors from "./app/config/colors"
+import HomeScreen from "./HomeScreen"
+import CoffeeDetailsScreen from "./app/screens/CoffeeDetailsScreen"
+import coffees from "./app/config/coffees"
 
 const App = () => {
-    return (
-        <View style={{flex: 1, backgroundColor: colors.dark}}>
-            <StatusBar style={"light"}/>
-            <HomeScreen/>
-        </View>
-    );
+  return (
+    <View style={{ flex: 1, backgroundColor: colors.dark }}>
+      <StatusBar style={"light"} />
+      {/*<HomeScreen/>*/}
+      <CoffeeDetailsScreen coffee={coffees[1]} />
+    </View>
+  )
 }
 export default App
 
