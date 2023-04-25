6787105317c5e7f933ed3f19c3a6490be2bc1840
Pavel <malinatrashh@gmail.com>
2023-04-25 22:18:42 +0800

    fix

--- App.js
+++ App.js
@@ -1,24 +1,17 @@
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
-import colors from './app/config/colors'
-import HomeScreen from "./HomeScreen";
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View } from "react-native"
+import colors from "./app/config/colors"
+import HomeScreen from "./HomeScreen"
+import CoffeeDetailsScreen from "./app/screens/CoffeeDetailsScreen"
+import coffees from "./app/config/coffees"
 
 const App = () => {
-<<<<<<< HEAD
   return (
     <View style={{ flex: 1, backgroundColor: colors.dark }}>
       <StatusBar style={"light"} />
       <HomeScreen/>
     </View>
   )
-=======
-    return (
-        <View style={{flex: 1, backgroundColor: colors.dark}}>
-            <StatusBar style={"light"}/>
-            <HomeScreen/>
-        </View>
-    );
->>>>>>> parent of 143e185 (Реализовал страницу с детальной информацией о кофе)
 }
 export default App
 
